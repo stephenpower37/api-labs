daa92cefb9ffdeb6fa17ac9e9a5dc88d5e3d46b8
Stephen Power <stephenxpower2003@gmail.com>
2022-12-02 16:29:04 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -14,28 +14,33 @@ router.get('/', async (req, res) => {
 
 // Register OR authenticate a user
 router.post('/',asyncHandler( async (req, res, next) => {
-    if (!req.body.username || !req.body.password) {
-      res.status(401).json({success: false, msg: 'Please pass username and password.'});
-      return next();
-    }
-    if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
-      const user = await User.findByUserName(req.body.username);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-        user.comparePassword(req.body.password, (err, isMatch) => {
-          if (isMatch && !err) {
-            // if user is found and password matches, create a token
-            const token = jwt.sign(user.username, process.env.SECRET);
-            // return the information including token as JSON
-            res.status(200).json({success: true, token: 'BEARER ' + token});
-          } else {
-            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
-          }
-        });
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({success: false, msg: 'Please pass username and password.'});
+    return next();
+  }
+  if (req.query.action === 'register') {
+      if (req.body.password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/)) {
+          await User.create(req.body);
+          res.status(201).json({ code: 201, msg: 'Successful created new user.' });
+      } else {
+          res.status(401).json({code:401, msg: 'Bad password, user not registered'})
       }
-  }));
+  } else {
+    const user = await User.findByUserName(req.body.username);
+      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          // if user is found and password matches, create a token
+          // eslint-disable-next-line no-undef
+          const token = jwt.sign(user.username, process.env.SECRET);
+          // return the information including token as JSON
+          res.status(200).json({success: true, token: 'BEARER ' + token});
+        } else {
+          res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+}));
 
   // Update a user
   router.put('/:id', async (req, res) => {
@@ -50,15 +55,20 @@ router.post('/',asyncHandler( async (req, res, next) => {
     }
 });
 
-//Add a favourite. No Error Handling Yet. Can add duplicates too!
+//Add favourites
 router.post('/:userName/favourites', asyncHandler(async (req, res) => {
   const newFavourite = req.body.id;
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+
+  if ( await user.favourites.includes(movie._id)){
+    res.status(401).json({code: 401,msg: 'Movie not added, movie already in favourites'});
+} else{
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+}
 }));
 
 router.get('/:userName/favourites', asyncHandler( async (req, res) => {
